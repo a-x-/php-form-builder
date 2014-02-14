d8efcbe756c656cad7227e83a374aacf063fde7c
Inventorem <invntrm@spbstu.ru>
2014-02-14 12:58:01 +0400

    Add class TextFormControl, class ButtonFormControl, class TextareaFormControl

--- FormControl.php
+++ FormControl.php
@@ -6,5 +6,36 @@
  */
 class FormControl
 {
+    private $_templates_collection_file;
 
-} 
+    function __construct($templates_collection_file)
+    {
+        $this->_templates_collection_file = $templates_collection_file;
+
+    }
+
+    function build()
+    {
+    }
+}
+
+class TextFormControl extends FormControl
+{
+
+}
+
+;
+
+class ButtonFormControl extends FormControl
+{
+
+}
+
+;
+
+class TextareaFormControl extends FormControl
+{
+
+}
+
+;
