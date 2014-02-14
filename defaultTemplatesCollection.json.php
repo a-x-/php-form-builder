373ea4cf4b86cd7104f84a63a1c19fc3cf6d52fe
Inventorem <invntrm@spbstu.ru>
2014-02-14 13:56:52 +0400

    Update *.json.php

--- defaultTemplatesCollection.json.php
+++ defaultTemplatesCollection.json.php
@@ -2,14 +2,14 @@
 "form"=> "
 <form action='' method='' enctype=''>%%form-content%%</form>",
 
-"base"=> "
+"text-base"=> "
 <div class='input-group'>
     <label class='input-group-addon' for='%%label%%'>%%caption%%</label>
     <input pattern='' title='' type='' name='%%label%%' id='%%label%%' class='form-control' placeholder='' %%attr%%>
 </div>",
 
-"submit"=> "
-<button type='submit' class='btn btn-primary'>%%submit_text%%</button>",
+"button-base"=> "
+<button type='' class='btn btn-primary'>%%value%%</button>",
 
 "option"=> "
 <option value=''>%%option%%</option>",
